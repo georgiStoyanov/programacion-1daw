85f0bce6fe75293a091ed0f04c363baf117ffcac
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2014-11-16 12:11:25 +0100

    Tengo en cuenta las múltiples filas de Pascal

--- PR-Correccion-array/src/main/java/TrianguloDePascal.java
+++ PR-Correccion-array/src/main/java/TrianguloDePascal.java
@@ -2,6 +2,9 @@
 public class TrianguloDePascal {
 
 	public static void main(String[] args) {
+		System.out.println( "1" );
+		System.out.println( "1 1" );
+		System.out.println( "1 2 1" );		
 		System.out.println( "1 3 3 1" );
 	}
 }
