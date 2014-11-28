14ff0a148dc75a743d737c03506deb7e7fa2041d
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2014-11-28 20:25:22 +0100

    Versión válida de ExtraerResultadosDeXML

--- PR-Correccion-array/src/main/scala/ExtraerResultadosDeXML.scala
+++ PR-Correccion-array/src/main/scala/ExtraerResultadosDeXML.scala
@@ -12,11 +12,14 @@ object ExtraerResultadosDeXML extends App{
 	}
 
 
-	case class ResultLine( student: String, test: String, totalTests: Int, failures : Int )
+	case class ResultLine( student: String, test: String, totalTests: Int, failures : Int ){
+		val valid = totalTests > 0
+	}
 
 	class Student( name: String ){
 		val results = mutable.Map[String,(Int,Int)]()
 		override def toString = name + "\t" + { for( (k,v) <- results ) yield k + "\t" + v._1 + "\t" + v._2 + "\t" }.mkString
+		def valid = results.size > 1
 	}
 
 	val regex = """(.*)TEST-.*name=(.*)tests="(\d+)".*failures="(\d+)".*""".r
@@ -35,13 +38,38 @@ object ExtraerResultadosDeXML extends App{
 	println( "**************************" )
 	println( results.mkString("\n") )
 
-	val resultsByStudent = mutable.Map[String,Student]()
+	type ResultsByStudent = mutable.Map[String,Student]
+
+	val resultsByStudent : ResultsByStudent = mutable.Map[String,Student]()
 	
-	for( result <- results ){
+	for( result <- results if result.valid ){
 		val student = resultsByStudent.getOrElseUpdate( result.student, new Student(result.student) )
 		student.results(result.test) = (result.totalTests, result.failures)
 	}
 
 	println( resultsByStudent.values.mkString("\n") )
 
+	def formatCSV( results : ResultsByStudent ) = {
+		val studentNames = results.keys.toArray.sorted
+		val firstStudent = results.values.head
+		val exercises = firstStudent.results.keys.toArray.sorted
+
+		val headerKeys = {
+			for( e <- exercises ; gb <- Seq(" total", " bad") ) yield e + gb
+		}
+
+		println( "\t" + headerKeys.mkString("\t" ) )
+
+		for( s <- studentNames ; student = results(s) if student.valid ){
+			print( s + "\t" )
+			for( e <- exercises ){
+				val test = student.results(e)
+				print( test._1 + "\t" + test._2 + "\t" ) 								
+			}
+			println("")
+		}
+	}
+
+	println( "\n" * 5 )		
+	formatCSV( resultsByStudent )
 }
