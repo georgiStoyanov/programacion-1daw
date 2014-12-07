f12665b82dbc290155ed550c60658f40f4e586bf
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2014-12-07 16:44:34 +0100

    Nuevos proyectos

---
+++ PR-Examen-Evaluacion1/src/test/java/SumaCapicua_soloSuma_test.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class SumaCapicua_soloSuma_test {
+
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
