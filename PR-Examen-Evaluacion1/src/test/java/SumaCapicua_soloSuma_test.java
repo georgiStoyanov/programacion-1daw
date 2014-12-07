ea72f81328b8c7d042bbfad0c4b2b40f2e36eba1
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2014-12-07 17:17:08 +0100

    Tests del examen

--- PR-Examen-Evaluacion1/src/test/java/SumaCapicua_soloSuma_test.java
+++ PR-Examen-Evaluacion1/src/test/java/SumaCapicua_soloSuma_test.java
@@ -7,12 +7,73 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class SumaCapicua_soloSuma_test {
+public class SumaCapicua_soloSuma_test extends InputOutputTest{
 
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void testSinIncorrectos() {
+	
+	setInputStream( "34\n33\n" );
+	invocaMain( "SumaCapicua", new String[0] );
+	String out = getLastLineOut();
+	
+	assertTrue( "34 y 33 son 67", out.matches(".*67.*" ) );
+	
     }
 
+    @Test
+    public void testIncorrectoUno() {
+	
+	setInputStream( "treinta y cuatro\n34\n33\n" );
+	invocaMain( "SumaCapicua", new String[0] );
+	String out = getLastLineOut();
+	
+	assertTrue( "34 y 33 son 67", out.matches(".*67.*" ) );
+	
+    }
+
+    @Test
+    public void testIncorrectoUnoUno() {
+	
+	setInputStream( "treinta y cuatro\nhola\n34\n33\n" );
+	invocaMain( "SumaCapicua", new String[0] );
+	String out = getLastLineOut();
+	
+	assertTrue( "34 y 33 son 67", out.matches(".*67.*" ) );
+	
+    }
+    
+    @Test
+    public void testIncorrectoDosDos() {
+	
+	setInputStream( "34\nhola\nhola\n33\n" );
+	invocaMain( "SumaCapicua", new String[0] );
+	String out = getLastLineOut();
+	
+	assertTrue( "34 y 33 son 67", out.matches(".*67.*" ) );
+	
+    }
+
+    
+    @Test
+    public void testIncorrectoDos() {
+	
+	setInputStream( "34\ntreinta y tres\n33\n" );
+	invocaMain( "SumaCapicua", new String[0] );
+	String out = getLastLineOut();
+	
+	assertTrue( "34 y 33 son 67", out.matches(".*67.*" ) );
+	
+    }
+
+    @Test
+    public void testIncorrectoUnoDos() {
+	
+	setInputStream( "treinta y cuatro\n34\ntreinta y tres\n33\n" );
+	invocaMain( "SumaCapicua", new String[0] );
+	String out = getLastLineOut();
+	
+	assertTrue( "34 y 33 son 67", out.matches(".*67.*" ) );
+    }
+    
 }
