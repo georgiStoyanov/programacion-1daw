2080364ba417750ab99e69ef346c2b7b9b10afd8
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2014-12-09 16:08:59 +0100

    Vaya lio con los branch :(

---
+++ examen/EstadisticaDeTexto.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * Realiza un programa que recoja una linea de texto por teclado (se supone que no habra signos de puntuacion).
+ * Despues imprimira la siguiente estadistica:
+ *  Numero de palabras (1 punto)
+ *  Longitud de la palabra mas larga (1.5 puntos)
+ *  Longitud de la palabra mas corta (1.5 puntos)
+ * Si no hubiese palabras que contabilizar, se indicara adecuadamente segun los ejemplos (1 punto)
+ * 
+ * @author ---nombre-del-alumno---
+ *
+ */
+public class EstadisticaDeTexto {
+    
+    public static void main(String[] args) {
+	
+    }
+}
---
+++ examen/SumaCapicua.java
@@ -0,0 +1,17 @@
+/**
+ * Realiza un programa que pregunte por dos numeros enteros y los sume. 
+ * Si no se introduce un numero, el
+ * programa seguira preguntando hasta que obtener un numero correcto (2 puntos). 
+ * Despues, este programa decidira si dicha suma es capicua (3 puntos)
+ * 
+ * 
+ * @author ---nombre-del-alumno---
+ *
+ */
+public class SumaCapicua {
+    
+    public static void main(String[] args) {
+	
+    }
+
+}
