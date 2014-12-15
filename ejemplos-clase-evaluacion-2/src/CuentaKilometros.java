0bed7a29a000f1521330b93953a0e2163fbe678d
alumno <alumno@iesavellaneda.org>
2014-12-15 19:01:08 +0100

    CuentaKilometros acabado

--- ejemplos-clase-evaluacion-2/src/CuentaKilometros.java
+++ ejemplos-clase-evaluacion-2/src/CuentaKilometros.java
@@ -1,7 +1,7 @@
 public class CuentaKilometros {
 
 	public static void printf(String format, Object... args) {
-		System.out.printf(format, args);
+		System.out.printf(format + "\n", args);
 	}
 
 	public static void main(String[] args) {
@@ -15,13 +15,13 @@ public class CuentaKilometros {
 		printf("hay que seguir con 0,0: %d,%d", 
 				ck.getTotalKilometros(), ck.getTotalHectometros());
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 110; i++) {
 			ck.addKilometros(0.01);
 		}
 
-		printf("hay que seguir con total 0,1: %d,%d", 
+		printf("hay que seguir con total 1,1: %d,%d", 
 				ck.getTotalKilometros(), ck.getTotalHectometros());
-		printf("hay que seguir con parcial 0,1: %d,%d",
+		printf("hay que seguir con parcial 1,1: %d,%d",
 				ck.getParcialKilometros(), ck.getParcialHectometros());
 
 		ck.resetearParcial();
@@ -30,5 +30,37 @@ public class CuentaKilometros {
 				ck.getTotalKilometros(), ck.getTotalHectometros());
 		printf("hay que seguir con parcial 0,0: %d,%d",
 				ck.getParcialKilometros(), ck.getParcialHectometros());
+	}
+
+	private double kilometrosTotales;
+	private double kilometrosParciales;
+	
+	private void resetearParcial() {
+		kilometrosParciales = 0;
+	}
+
+	private int getParcialHectometros() {
+		return dameElPrimerDecimal(kilometrosParciales);
+	}
+
+	private int dameElPrimerDecimal(double d) {
+		return ((int)(d*10))%10;
+	}
+
+	private int getParcialKilometros() {
+		return (int) Math.floor(kilometrosParciales);
+	}
+
+	private void addKilometros(double kilometros) {
+		kilometrosParciales += kilometros;
+		kilometrosTotales += kilometros;
+	}
+
+	private int getTotalHectometros() {
+		return dameElPrimerDecimal(kilometrosTotales);
+	}
+
+	private int getTotalKilometros() {
+		return (int) Math.floor(kilometrosTotales);
 	}
 }
