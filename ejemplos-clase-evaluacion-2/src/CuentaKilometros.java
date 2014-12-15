07eecfe64e7c0ac1f5171541efb33c596b31c3a2
alumno <alumno@iesavellaneda.org>
2014-12-15 19:10:49 +0100

    Ahora con toString

--- ejemplos-clase-evaluacion-2/src/CuentaKilometros.java
+++ ejemplos-clase-evaluacion-2/src/CuentaKilometros.java
@@ -7,34 +7,35 @@ public class CuentaKilometros {
 	public static void main(String[] args) {
 		CuentaKilometros ck = new CuentaKilometros();
 
-		printf("hay que empezar por 0,0: %d,%d", 
-				ck.getTotalKilometros(), ck.getTotalHectometros());
+		printf("hay que empezar por 0,0:" + ck );
 
 		ck.addKilometros(0.01);
 
-		printf("hay que seguir con 0,0: %d,%d", 
-				ck.getTotalKilometros(), ck.getTotalHectometros());
+		printf("hay que seguir con 0,0:" + ck);
 
 		for (int i = 0; i < 110; i++) {
 			ck.addKilometros(0.01);
 		}
 
-		printf("hay que seguir con total 1,1: %d,%d", 
-				ck.getTotalKilometros(), ck.getTotalHectometros());
-		printf("hay que seguir con parcial 1,1: %d,%d",
-				ck.getParcialKilometros(), ck.getParcialHectometros());
+		printf("hay que seguir con total 1,1" + ck);
+		printf("hay que seguir con parcial 1,1:" + ck);
 
 		ck.resetearParcial();
 
-		printf("hay que seguir con total 0,1: %d,%d", 
-				ck.getTotalKilometros(), ck.getTotalHectometros());
-		printf("hay que seguir con parcial 0,0: %d,%d",
-				ck.getParcialKilometros(), ck.getParcialHectometros());
+		printf("hay que seguir con total 1,1:" + ck);
+		printf("hay que seguir con parcial 0,0:" + ck);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Total:%d,%d  Parcial:%d,%d",
+				getTotalKilometros(), getTotalHectometros(),
+				getParcialKilometros(), getParcialHectometros());
 	}
 
 	private double kilometrosTotales;
 	private double kilometrosParciales;
-	
+
 	private void resetearParcial() {
 		kilometrosParciales = 0;
 	}
@@ -44,7 +45,7 @@ public class CuentaKilometros {
 	}
 
 	private int dameElPrimerDecimal(double d) {
-		return ((int)(d*10))%10;
+		return ((int) (d * 10)) % 10;
 	}
 
 	private int getParcialKilometros() {
