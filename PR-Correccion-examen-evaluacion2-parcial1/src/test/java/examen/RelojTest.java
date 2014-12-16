fc27e840eca20695ec6ce6aa39522b8bbaf00243
alumno <alumno@iesavellaneda.org>
2014-12-16 04:35:00 +0100

    Más test de reloj

--- PR-Correccion-examen-evaluacion2-parcial1/src/test/java/examen/RelojTest.java
+++ PR-Correccion-examen-evaluacion2-parcial1/src/test/java/examen/RelojTest.java
@@ -52,152 +52,179 @@ public class RelojTest {
 					+ " no tiene ese segundo", r.getSegundos() == i);
 		}
 	}
+
+	@Test
+	void cambiadoAHoraMenorDeCero() {
+		boolean exception = false;
+		try {
+			Reloj r = new Reloj();
+			r.setHoras(-1);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia cambiarse un reloj con hora menor de 0",
+				exception);
+	}
+
+	@Test
+	void cambiadoAHoraMayorDe23() {
+		boolean exception = false;
+		try {
+			Reloj r = new Reloj();
+			r.setHoras(24);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia cambiarse un reloj con hora mayor de 23",
+				exception);
+	}
+
+	@Test
+	void cambiadoAMinutoMenorDeCero() {
+		boolean exception = false;
+		try {
+			Reloj r = new Reloj();
+			r.setMinutos(-1);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia cambiarse un reloj con minuto negativo",
+				exception);
+	}
+
+	@Test
+	void cambiadoAMinutoMayorDe59() {
+		boolean exception = false;
+		try {
+			new Reloj().setMinutos(60);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia cambiarse un reloj con minuto mayor de 59",
+				exception);
+	}
+
+	@Test
+	void cambiadoASegundoMenorDeCero() {
+		boolean exception = false;
+		try {
+			new Reloj().setSegundos(-1);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia cambiarse un reloj con segundo negativo",
+				exception);
+	}
+
+	@Test
+	void cambiadoASegundoMayorDe59() {
+		boolean exception = false;
+		try {
+			new Reloj().setSegundos(60);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia cambiarse un reloj con segundo mayor de 59",
+				exception);
+	}
+
+	@Test
+	void creadoEnHoraMenorDeCero() {
+		boolean exception = false;
+		try {
+			new Reloj(-1, 0, 0);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia crearse un reloj con hora negativa", exception);
+	}
+
+	@Test
+	void creadoEnHoraMayorDe23() {
+		boolean exception = false;
+		try {
+			new Reloj(24, 0, 0);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia crearse un reloj con hora mayor de 23",
+				exception);
+	}
+
+	@Test
+	void creadoEnMinutoMenorDeCero() {
+		boolean exception = false;
+		try {
+			new Reloj(0, -1, 0);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia crearse un reloj con minuto negativo", exception);
+	}
+
+	@Test
+	void creadoEnMinutoMayorDe59() {
+		boolean exception = false;
+		try {
+			new Reloj(0, 60, 0);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia crearse un reloj con minuto mayor de 59",
+				exception);
+	}
+
+	@Test
+	void creadoEnSegundoMenorDeCero() {
+		boolean exception = false;
+		try {
+			new Reloj(0, 0, -1);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia crearse un reloj con segundo negativo",
+				exception);
+	}
+
+	@Test
+	void creadoEnSegundoMayorDe59() {
+		boolean exception = false;
+		try {
+			new Reloj(0, 0, 60);
+		} catch (Exception e) {
+			exception = true;
+		}
+		assertTrue("No deberia crearse un reloj con segundo mayor de 59",
+				exception);
+	}
+
+	private static String cadena(int hora, int minuto, int segundo) {
+		return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+	}
+
+	@Test
+	void segundosRotanConUnTick() {
+		Reloj r = new Reloj(0,0,59);
+		r.tick();
+		assertTrue( "Los segundos deben rotar de 59 a 0 con tick", r.getSegundos() == 0 );
+		assertTrue( "Los minutos deben avanzar al rotar los segundos de 59 a 0 con tick", r.getMinutos() == 1 );
+	}
+
+	@Test
+	void minutosRotanConUnTick() {
+		Reloj r = new Reloj(0,59,59);
+		r.tick();
+		assertTrue( "Los minutos deben rotar de 59 a 0 con tick ", r.getMinutos() == 0 );
+		assertTrue( "Las horas deben avanzar al rotar los minutos de 59 a 0 con tick", r.getHoras() == 1 );
+	}
+
+	@Test
+	void horasRotanConUnTick() {
+		Reloj r = new Reloj(23,59,59);
+		r.tick();
+		assertTrue( "Las horas deben rotar de 23 a 0 con tick", r.getHoras() == 0 );
+	}
 	
-	@Test
-	void creadoEnHoraMenorDeCero(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj( -1,0,0);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia crearse un reloj con hora negativa", exception);
-	}
-
-	@Test
-	void cambiadoAHoraMenorDeCero(){
-		  boolean exception = false;
-	      try {
-	    	  Reloj r = new Reloj();
-	    	  r.setHoras(-1);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia cambiarse un reloj con hora menor de 0", exception);
-	}
-
-	@Test
-	void cambiadoAHoraMayorDe23(){
-		  boolean exception = false;
-	      try {
-	    	  Reloj r = new Reloj();
-	    	  r.setHoras(24);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia cambiarse un reloj con hora mayor de 23", exception);
-	}
-
-	@Test
-	void cambiadoAMinutoMenorDeCero(){
-		  boolean exception = false;
-	      try {
-	    	  Reloj r = new Reloj();
-	    	  r.setMinutos(-1);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia cambiarse un reloj con minuto negativo", exception);
-	}
-
-	@Test
-	void cambiadoAMinutoMayorDe59(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj().setMinutos(60);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia cambiarse un reloj con minuto mayor de 59", exception);
-	}
-
-	@Test
-	void cambiadoASegundoMenorDeCero(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj().setSegundos(-1);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia cambiarse un reloj con segundo negativo", exception);
-	}
-
-	@Test
-	void cambiadoASegundoMayorDe59(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj().setSegundos(60);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia cambiarse un reloj con segundo mayor de 59", exception);
-	}
-
-	@Test
-	void creadoEnHoraMayorDe23(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj( 24,0,0);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia crearse un reloj con hora mayor de 23", exception);
-	}
-
-	@Test
-	void creadoEnMinutoMenorDeCero(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj( 0,-1,0);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia crearse un reloj con minuto negativo", exception);
-	}
-
-	@Test
-	void creadoEnMinutoMayorDe59(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj( 0,60,0);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia crearse un reloj con minuto mayor de 59", exception);
-	}
-
-	@Test
-	void creadoEnSegundoMenorDeCero(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj( 0,0,-1);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia crearse un reloj con segundo negativo", exception);
-	}
-
-	@Test
-	void creadoEnSegundoMayorDe59(){
-		  boolean exception = false;
-	      try {
-	    	  new Reloj( 0,0,60);
-	      }
-	      catch (Exception e) {
-	          exception = true;
-	      }
-	      assertTrue("No deberia crearse un reloj con segundo mayor de 59", exception);
-	}
 	
+
 }
