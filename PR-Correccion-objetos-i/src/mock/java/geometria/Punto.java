172ab8137d58cc4b2fe8986046417caf27aa13fc
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2015-01-08 10:50:47 +0100

    Borrado de soluciones propuestas

--- PR-Correccion-objetos-i/src/mock/java/geometria/Punto.java
+++ PR-Correccion-objetos-i/src/mock/java/geometria/Punto.java
@@ -2,41 +2,28 @@ package geometria;
 
 public class Punto {
 
-    private double _x;
-    private double _y;
 
     public Punto() {
     }
 
     public Punto(double x, double y) {
-        _x = x;
-        _y = y;
 
     }
 
     public double getX() {
-        return _x;
-
     }
 
     public double getY() {
-        return _y;
-
     }
 
     public double distanciaHasta(Punto p) {
-        double dx = getX() - p.getX();
-        double dy = getY() - p.getY();
-        return Math.sqrt(dx * dx + dy * dy);
 
     }
 
     public Punto suma(Punto p) {
-        return new Punto(p.getX() + getX(), p.getY() + getY());
     }
 
     @Override
     public String toString() {
-        return "(" + getX() + " , " + getY() + ")";
     }
 }
--- PR-Correccion-objetos-i/src/mock/java/geometria/Rectangulo.java
+++ PR-Correccion-objetos-i/src/mock/java/geometria/Rectangulo.java
@@ -2,69 +2,35 @@ package geometria;
 
 public class Rectangulo {
 
-    private Punto _centro;
-    private double _alto;
-    private double _ancho;
 
     public Rectangulo(Punto centro, double alto, double ancho) {
-        setCentro(centro);
-        setAlto(alto);
-        setAncho(ancho);
     }
 
     public Punto getCentro() {
-        return _centro;
     }
 
     public void setCentro(Punto p) {
-        _centro = p;
     }
 
     public double getAlto() {
-        return _alto;
     }
 
     public void setAlto(double alto) {
-        if( alto < 0 ){
-            throw new IllegalArgumentException( "alto negativo:" + alto );
-        }
-        _alto = alto;
     }
 
     public double getAncho() {
-        return _ancho;
     }
 
     public void setAncho(double ancho) {
-        if( ancho < 0 ){
-            throw new IllegalArgumentException( "ancho negativo:" + ancho );
-        }
-        _ancho = ancho;
     }
 
     public Punto[] getEsquinas() {
-        Punto c = getCentro();
-        double x = c.getX();
-        double y = c.getY();
-        double al = getAlto()/2;
-        double an = getAncho()/2;
-        return new Punto[]{
-                new Punto( x - an, y + al ),
-                new Punto( x + an, y + al),
-                new Punto( x - an, y - al),
-                new Punto( x + an, y - al),
-        };
     }
 
     public double getArea() {
-        return getAlto() * getAncho();
     }
     
     @Override
     public String toString() {
-        Punto[] esquinas = getEsquinas();
-        Punto ari = esquinas[0];
-        Punto abd = esquinas[3];
-        return "arribaizquierda:" + ari + " abajoderecha:" + abd;
     }
 }
