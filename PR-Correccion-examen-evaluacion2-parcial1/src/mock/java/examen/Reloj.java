78f0299b0a4b14deb0fbe8dcb658a3fd67d0b08d
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2015-01-13 13:38:04 +0100

    Pruebas "definitivas" del examen

--- PR-Correccion-examen-evaluacion2-parcial1/src/mock/java/examen/Reloj.java
+++ PR-Correccion-examen-evaluacion2-parcial1/src/mock/java/examen/Reloj.java
@@ -5,77 +5,64 @@ package examen;
  */
 public class Reloj{
 	
+    private int t = 0;
+    
 	/**
 	 * Crea un reloj en la hora 00:00:00
 	 */
 	public Reloj(){
-		throw new UnsupportedOperationException("Por hacer");
 	}
 
 	/**
  	 * Crea un reloj con horas, minutos y segundos especificados
 	 */
 	public Reloj( int horas, int minutos, int segundos ){
-		throw new UnsupportedOperationException("Por hacer");
+	    if( horas <0 || horas > 23 ) throw new IllegalArgumentException();
+        if( minutos <0 || minutos > 59 ) throw new IllegalArgumentException();
+        if( segundos <0 || segundos > 59 ) throw new IllegalArgumentException();
+        t = horas*60*60 + minutos*60 + segundos;
 	}
 
 	/**
 	 * @return 0 a 23
 	 */
 	public int getHoras(){
-		throw new UnsupportedOperationException("Por hacer");
+	    return t/(60*60);
 	}
 
 	/**
 	 * @return 0 a 59
 	 */
 	public int getMinutos(){
-		throw new UnsupportedOperationException("Por hacer");
+	    return (t/60) % 60;
 	}
 
 	/**
 	 * @return 0 a 59
 	 */
 	public int getSegundos(){
-		throw new UnsupportedOperationException("Por hacer");
-	}
-
-	/**
-	 * @param horas 0 a 23
-	 * @throws InvalidArgumentException
-	 */
-	public void setHoras(int horas){
-		throw new UnsupportedOperationException("Por hacer");
-	}
-
-	/**
-	 * @param minutos 0 a 59
-	 * @throws InvalidArgumentException
-	 */
-	public void setMinutos(int minutos){
-		throw new UnsupportedOperationException("Por hacer");
-	}
-
-	/**
-	 * @param segundos 0 a 59
-	 * @throws InvalidArgumentException
-	 */
-	public void setSegundos(int segundos){
-		throw new UnsupportedOperationException("Por hacer");
+	    return t%60;
 	}
 
 	/**
 	 * Avanza un segundo
 	 */
 	public void tick(){
-		throw new UnsupportedOperationException("Por hacer");
+	    t += 1;
+	    t %= 60*60*24;
 	}
 
 	/**
 	 * Avanza las horas, minutos y segundos especificados (cada uno no debe superar las 100 unidades, y no deben ser negativos)
 	 */
 	public void tick(int horas, int minutos, int segundos){
-		throw new UnsupportedOperationException("Por hacer");
+        if( horas <0 || horas > 100 ) throw new IllegalArgumentException();
+        if( minutos <0 || minutos > 100 ) throw new IllegalArgumentException();
+        if( segundos <0 || minutos > 100 ) throw new IllegalArgumentException();
+        
+        for( int s = 0 ; s < horas*60*60 + minutos*60 + segundos ; s++ ){
+            tick();
+        }
 	}
 
 
@@ -84,6 +71,6 @@ public class Reloj{
 	 */
 	@Override
 	public String toString(){
-		throw new UnsupportedOperationException("Por hacer");
+		return String.format( "%02d:%02d:%02d", getHoras(), getMinutos(), getSegundos() );
 	}	
 }
--- PR-Correccion-examen-evaluacion2-parcial1/src/test/java/examen/RelojTest.java
+++ PR-Correccion-examen-evaluacion2-parcial1/src/test/java/examen/RelojTest.java
@@ -8,295 +8,258 @@ import org.junit.rules.Timeout;
 
 public class RelojTest {
 
-	@Rule
-	public Timeout globalTimeout = new Timeout(10000);
+    @Rule
+    public Timeout globalTimeout = new Timeout(10000);
 
-	@Test
-	public void relojPorDefectoCreadoEnMedianoche() {
-		Reloj r = new Reloj();
+    @Test
+    public void relojPorDefectoCreadoEnMedianoche() {
+        Reloj r = new Reloj();
 
-		assertTrue("El reloj por defecto se crea en medianoche",
-				r.getHoras() == 0);
-		assertTrue("El reloj por defecto se crea en medianoche",
-				r.getMinutos() == 0);
-		assertTrue("El reloj por defecto se crea en medianoche",
-				r.getSegundos() == 0);
-	}
+        assertTrue("El reloj por defecto se crea en medianoche",
+                r.getHoras() == 0);
+        assertTrue("El reloj por defecto se crea en medianoche",
+                r.getMinutos() == 0);
+        assertTrue("El reloj por defecto se crea en medianoche",
+                r.getSegundos() == 0);
+    }
 
-	@Test
-	public void relojCreadoEnHora() {
-		for (int i = 0; i < 23; i++) {
-			Reloj r = new Reloj(i, 0, 0);
+    @Test
+    public void relojCreadoEnHora() {
+        for (int i = 0; i < 23; i++) {
+            Reloj r = new Reloj(i, 0, 0);
 
-			assertTrue("El reloj creado con hora " + i + " no tiene esa hora",
-					r.getHoras() == i);
-		}
-	}
+            assertTrue("El reloj creado con hora " + i + " no tiene esa hora",
+                    r.getHoras() == i);
+        }
+    }
 
-	@Test
-	public void relojCreadoEnMinuto() {
-		for (int i = 0; i < 60; i++) {
-			Reloj r = new Reloj(0, i, 0);
+    @Test
+    public void relojCreadoEnMinuto() {
+        for (int i = 0; i < 60; i++) {
+            Reloj r = new Reloj(0, i, 0);
 
-			assertTrue("El reloj creado con minuto " + i
-					+ " no tiene ese minuto", r.getMinutos() == i);
-		}
-	}
+            assertTrue("El reloj creado con minuto " + i
+                    + " no tiene ese minuto", r.getMinutos() == i);
+        }
+    }
 
-	@Test
-	public void relojCreadoEnSegundo() {
-		for (int i = 0; i < 60; i++) {
-			Reloj r = new Reloj(0, 0, i);
+    @Test
+    public void relojCreadoEnSegundo() {
+        for (int i = 0; i < 60; i++) {
+            Reloj r = new Reloj(0, 0, i);
 
-			assertTrue("El reloj creado con segundo " + i
-					+ " no tiene ese segundo", r.getSegundos() == i);
-		}
-	}
+            assertTrue("El reloj creado con segundo " + i
+                    + " no tiene ese segundo", r.getSegundos() == i);
+        }
+    }
 
-	@Test
-	void cambiadoAHoraMenorDeCero() {
-		boolean exception = false;
-		try {
-			Reloj r = new Reloj();
-			r.setHoras(-1);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia cambiarse un reloj con hora menor de 0",
-				exception);
-	}
+    @Test
+    public void creadoEnHoraMenorDeCero() {
+        boolean exception = false;
+        try {
+            new Reloj(-1, 0, 0);
+        }
+        catch (Exception e) {
+            exception = true;
+        }
+        assertTrue("No deberia crearse un reloj con hora negativa", exception);
+    }
 
-	@Test
-	void cambiadoAHoraMayorDe23() {
-		boolean exception = false;
-		try {
-			Reloj r = new Reloj();
-			r.setHoras(24);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia cambiarse un reloj con hora mayor de 23",
-				exception);
-	}
+    @Test
+    public void creadoEnHoraMayorDe23() {
+        boolean exception = false;
+        try {
+            new Reloj(24, 0, 0);
+        }
+        catch (Exception e) {
+            exception = true;
+        }
+        assertTrue("No deberia crearse un reloj con hora mayor de 23",
+                exception);
+    }
 
-	@Test
-	void cambiadoAMinutoMenorDeCero() {
-		boolean exception = false;
-		try {
-			Reloj r = new Reloj();
-			r.setMinutos(-1);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia cambiarse un reloj con minuto negativo",
-				exception);
-	}
+    @Test
+    public void creadoEnMinutoMenorDeCero() {
+        boolean exception = false;
+        try {
+            new Reloj(0, -1, 0);
+        }
+        catch (Exception e) {
+            exception = true;
+        }
+        assertTrue("No deberia crearse un reloj con minuto negativo", exception);
+    }
 
-	@Test
-	void cambiadoAMinutoMayorDe59() {
-		boolean exception = false;
-		try {
-			new Reloj().setMinutos(60);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia cambiarse un reloj con minuto mayor de 59",
-				exception);
-	}
+    @Test
+    public void creadoEnMinutoMayorDe59() {
+        boolean exception = false;
+        try {
+            new Reloj(0, 60, 0);
+        }
+        catch (Exception e) {
+            exception = true;
+        }
+        assertTrue("No deberia crearse un reloj con minuto mayor de 59",
+                exception);
+    }
 
-	@Test
-	void cambiadoASegundoMenorDeCero() {
-		boolean exception = false;
-		try {
-			new Reloj().setSegundos(-1);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia cambiarse un reloj con segundo negativo",
-				exception);
-	}
+    @Test
+    public void creadoEnSegundoMenorDeCero() {
+        boolean exception = false;
+        try {
+            new Reloj(0, 0, -1);
+        }
+        catch (Exception e) {
+            exception = true;
+        }
+        assertTrue("No deberia crearse un reloj con segundo negativo",
+                exception);
+    }
 
-	@Test
-	void cambiadoASegundoMayorDe59() {
-		boolean exception = false;
-		try {
-			new Reloj().setSegundos(60);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia cambiarse un reloj con segundo mayor de 59",
-				exception);
-	}
+    @Test
+    public void creadoEnSegundoMayorDe59() {
+        boolean exception = false;
+        try {
+            new Reloj(0, 0, 60);
+        }
+        catch (Exception e) {
+            exception = true;
+        }
+        assertTrue("No deberia crearse un reloj con segundo mayor de 59",
+                exception);
+    }
 
-	@Test
-	void creadoEnHoraMenorDeCero() {
-		boolean exception = false;
-		try {
-			new Reloj(-1, 0, 0);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia crearse un reloj con hora negativa", exception);
-	}
+    private static String cadena(int hora, int minuto, int segundo) {
+        return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+    }
 
-	@Test
-	void creadoEnHoraMayorDe23() {
-		boolean exception = false;
-		try {
-			new Reloj(24, 0, 0);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia crearse un reloj con hora mayor de 23",
-				exception);
-	}
+    @Test
+    public void segundosRotanConUnTick() {
+        Reloj r = new Reloj(0, 0, 59);
+        r.tick();
+        assertTrue("Los segundos deben rotar de 59 a 0 con tick",
+                r.getSegundos() == 0);
+        assertTrue(
+                "Los minutos deben avanzar al rotar los segundos de 59 a 0 con tick",
+                r.getMinutos() == 1);
+    }
 
-	@Test
-	void creadoEnMinutoMenorDeCero() {
-		boolean exception = false;
-		try {
-			new Reloj(0, -1, 0);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia crearse un reloj con minuto negativo", exception);
-	}
+    @Test
+    public void minutosRotanConUnTick() {
+        Reloj r = new Reloj(0, 59, 59);
+        r.tick();
+        assertTrue("Los minutos deben rotar de 59 a 0 con tick ",
+                r.getMinutos() == 0);
+        assertTrue(
+                "Las horas deben avanzar al rotar los minutos de 59 a 0 con tick",
+                r.getHoras() == 1);
+    }
 
-	@Test
-	void creadoEnMinutoMayorDe59() {
-		boolean exception = false;
-		try {
-			new Reloj(0, 60, 0);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia crearse un reloj con minuto mayor de 59",
-				exception);
-	}
+    @Test
+    public void horasRotanConUnTick() {
+        Reloj r = new Reloj(23, 59, 59);
+        r.tick();
+        assertTrue("Las horas deben rotar de 23 a 0 con tick",
+                r.getHoras() == 0);
+    }
 
-	@Test
-	void creadoEnSegundoMenorDeCero() {
-		boolean exception = false;
-		try {
-			new Reloj(0, 0, -1);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia crearse un reloj con segundo negativo",
-				exception);
-	}
+    @Test
+    public void representacionComoCadenaEnCreacion() {
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m < 60; m++) {
+                for (int s = 0; s < 60; s++) {
+                    Reloj r = new Reloj(h, m, s);
+                    assertTrue(
+                            "Representacion de cadena incorrecta al crear un reloj con h:"
+                                    + h + " m:" + m + " s:" + s,
+                            cadena(h, m, s).equals(r.toString()));
+                }
+            }
+        }
+    }
 
-	@Test
-	void creadoEnSegundoMayorDe59() {
-		boolean exception = false;
-		try {
-			new Reloj(0, 0, 60);
-		} catch (Exception e) {
-			exception = true;
-		}
-		assertTrue("No deberia crearse un reloj con segundo mayor de 59",
-				exception);
-	}
+    
+    @Test
+    public void representacionComoCadenaConTickCompuesto() {
+        for (int h = 0; h < 24; h += 5) {
+            for (int m = 0; m < 60; m += 5) {
+                for (int s = 0; s < 60; s += 5) {
+                    Reloj r = new Reloj();
+                    r.tick(h, m, s);
+                    assertTrue(
+                            "Representacion de cadena incorrecta al avanzar  un reloj con tick h:"
+                                    + h + " m:" + m + " s:" + s + " --- " + r.toString(),
+                            cadena(h, m, s).equals(r.toString()));
+                }
+            }
+        }
+    }
 
-	private static String cadena(int hora, int minuto, int segundo) {
-		return String.format("%02d:%02d:%02d", hora, minuto, segundo);
-	}
+    @Test
+    public void tickCompuesto2(){
+        tickCompuesto();
+    }
+    
+    @Test
+    public void tickCompuesto() {
+        for (int h = 0; h < 24; h += 5) {
+            for (int m = 0; m < 60; m += 5) {
+                for (int s = 0; s < 60; s += 5) {
+                    Reloj r = new Reloj();
+                    r.tick(h, m, s);
+                    assertTrue( "los segundos deberían ser " + s + " tras tick("+ h + "," + m + "," + s + "):" + r.getSegundos() + " -- " + r.toString(), r.getSegundos() == s );
+                    assertTrue( "los minutos deberían ser " + m + " tras tick("+ h + "," + m + "," + s + "):" + r.getMinutos() + " -- " + r.toString(), r.getMinutos() == m );
+                    assertTrue( "los horas deberían ser " + h + " tras tick("+ h + "," + m + "," + s + "):" + r.getHoras() + " -- " + r.toString(), r.getHoras() == h );
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void representacionComoCadenaConTickSimple() {
+        for (int h = 0; h < 24; h += 4) {
+            for (int m = 0; m < 60; m += 4) {
+                for (int s = 0; s < 60; s += 4) {
+                    Reloj r = new Reloj();
+                    for (int t = 0; t < h * 60 * 60 + m * 60 + s; t++) {
+                        r.tick();
+                    }
+                    assertTrue(
+                            "Representacion de cadena incorrecta al avanzar un reloj varias veces con tick h:"
+                                    + h + " m:" + m + " s:" + s,
+                            cadena(h, m, s).equals(r.toString()));
+                }
+            }
+        }
+    }
+    
+    
+    @Test
+    public void tickSimple2(){
+        tickSimple();
+    }
 
-	@Test
-	void segundosRotanConUnTick() {
-		Reloj r = new Reloj(0, 0, 59);
-		r.tick();
-		assertTrue("Los segundos deben rotar de 59 a 0 con tick",
-				r.getSegundos() == 0);
-		assertTrue(
-				"Los minutos deben avanzar al rotar los segundos de 59 a 0 con tick",
-				r.getMinutos() == 1);
-	}
+    @Test
+    public void tickSimple3(){
+        tickSimple();
+    }
 
-	@Test
-	void minutosRotanConUnTick() {
-		Reloj r = new Reloj(0, 59, 59);
-		r.tick();
-		assertTrue("Los minutos deben rotar de 59 a 0 con tick ",
-				r.getMinutos() == 0);
-		assertTrue(
-				"Las horas deben avanzar al rotar los minutos de 59 a 0 con tick",
-				r.getHoras() == 1);
-	}
+    @Test
+    public void tickSimple() {
+        for (int h = 0; h < 24; h += 5) {
+            for (int m = 0; m < 60; m += 5) {
+                for (int s = 0; s < 60; s += 5) {
+                    Reloj r = new Reloj();
+                    int ticks = h * 60 * 60 + m * 60 + s;
+                    for( int t = 0 ; t < ticks ; t += 1 ){
+                        r.tick();
+                    }
+                    assertTrue( "los segundos deberían ser " + s + " tras " + ticks + " ticks: " + r.getSegundos() + " -- " + r.toString(), r.getSegundos() == s );
+                    assertTrue( "los minutos deberían ser " + m + " tras " + ticks + " ticks: " + r.getMinutos() + " -- " + r.toString(), r.getMinutos() == m );
+                    assertTrue( "los horas deberían ser " + h + " tras " + ticks + " ticks: " + r.getHoras() + " -- " + r.toString(), r.getHoras() == h );
+                }
+            }
+        }
+    }
 
-	@Test
-	void horasRotanConUnTick() {
-		Reloj r = new Reloj(23, 59, 59);
-		r.tick();
-		assertTrue("Las horas deben rotar de 23 a 0 con tick",
-				r.getHoras() == 0);
-	}
-
-	@Test
-	void representacionComoCadenaEnCreacion() {
-		for (int h = 0; h < 24; h++) {
-			for (int m = 0; m < 60; m++) {
-				for (int s = 0; s < 60; s++) {
-					Reloj r = new Reloj(h, m, s);
-					assertTrue(
-							"Representacion de cadena incorrecta al crear un reloj con h:"
-									+ h + " m:" + m + " s:" + s,
-							cadena(h, m, s).equals(r.toString()));
-				}
-			}
-		}
-	}
-	
-	@Test
-	void representacionComoCadenaConSet() {
-		for (int h = 0; h < 24; h++) {
-			for (int m = 0; m < 60; m++) {
-				for (int s = 0; s < 60; s++) {
-					Reloj r = new Reloj();
-					r.setHoras(h);
-					r.setMinutos(m);
-					r.setSegundos(s);
-					assertTrue(
-							"Representacion de cadena incorrecta al cambiar un reloj con h:"
-									+ h + " m:" + m + " s:" + s,
-							cadena(h, m, s).equals(r.toString()));
-				}
-			}
-		}
-	}
-
-	@Test
-	void representacionComoCadenaConTick() {
-		for (int h = 0; h < 24; h++) {
-			for (int m = 0; m < 60; m++) {
-				for (int s = 0; s < 60; s++) {
-					Reloj r = new Reloj();
-					r.tick(h,m,s);
-					assertTrue(
-							"Representacion de cadena incorrecta al avanzar  un reloj con tick h:"
-									+ h + " m:" + m + " s:" + s,
-							cadena(h, m, s).equals(r.toString()));
-				}
-			}
-		}
-	}
-
-	@Test
-	void representacionComoCadenaConTickSimple() {
-		for (int h = 0; h < 24; h++) {
-			for (int m = 0; m < 60; m++) {
-				for (int s = 0; s < 60; s++) {
-					Reloj r = new Reloj();
-					for( int t = 0 ; t < h*60*60 + m*60 + s ; t ++ ){
-						r.tick();
-					}
-					assertTrue(
-							"Representacion de cadena incorrecta al avanzar un reloj varias veces con tick h:"
-									+ h + " m:" + m + " s:" + s,
-							cadena(h, m, s).equals(r.toString()));
-				}
-			}
-		}
-	}
-	
 }
