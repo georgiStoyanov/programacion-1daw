8ba46a152788fb1144ae03565432d9bab17de8b3
alvarogonzalezprofesor@gmail.com <alvarogonzalezprofesor@gmail.com>
2015-01-23 18:54:41 +0100

    Con vi en el cole

--- PR-Correcion-objetos-ii/src/main/java/banco/ProductoBancario.java
+++ PR-Correcion-objetos-ii/src/main/java/banco/ProductoBancario.java
@@ -10,24 +10,38 @@ public class ProductoBancario{
      *
      */
     public ProductoBancario( String titular, int codigo ){
+	_titular = titular;
+	_codigo = codigo;
+	_saldo = 0;
     }
 
     /**
      * Saldo actual del producto (positivo, a favor del cliente)
      */
     public double getSaldo(){
+	return _saldo;
     }
 
     /**
      * El codigo del producto
      */
     public int getCodigo(){
+	return _codigo;
     }
+       
+    /**
+     * El titular del producto
+     */
+    public String getTitular(){
+       return _titular;
+    }
+
 
     /**
      * Movimiento de saldo, a sumar al saldo. del producto.
      */
     public void movimientoSaldo( double movimiento ){
+       _saldo += movimiento;
     }
 
     /**
@@ -35,6 +49,10 @@ public class ProductoBancario{
      */
     @Override
     public boolean equals( Object o ){
+       if( !(o instanceof ProductoBancario) ){
+          return false;
+       }
+       ProductoBancario pb = (ProductoBancario)o;
+       return o.getTitular().equals( getTitular() ) && o.getCodigo() == getCodigo() && o.getClass().equals( getClass() );
     }
-
 }
--- PR-Correcion-objetos-ii/src/main/java/banco/TerminalBancario.java
+++ PR-Correcion-objetos-ii/src/main/java/banco/TerminalBancario.java
@@ -6,23 +6,40 @@ package banco;
  */
 public class TerminalBancario{
 
+    private Lista _productos = new Lista();
 
     /**
      * Lista de todos los productos del banco
      */
     public Lista productos(){
+       return _productos;
     }
 
     /**
      * Lista de los productos de un titular 
      */
     public Lista productosDeTitular( String titular ){
+       Lista ret = new Lista();
+       Lista p = productos();
+       for( int i = 0 ; i < p.numero() ; i += 1 ){
+         ProductoBancario pb = (ProductoBancario)p.objetoEn(i);
+         if( pb.getTitular().equals( titular ) ){
+           ret.agrega( pb );
+         }
+       }
+       return ret;
     }
 
     /**
      * Saldo total del titular (la suma de los saldos de sus productos)
      */
     public double saldoDeTitular( String titular ){
+       Lista p = productosDeTitular(titular);
+       double ret = 0;
+       for( int i = 0 ; i < p.numero(); i += 1 ){
+         ret += p.objetoEn(i).saldo();
+       }
+       return ret;
     }
 
     /**
@@ -30,6 +47,26 @@ public class TerminalBancario{
      * @returns Si el producto no existia, devuelve false.
      */
     public boolean cancelaProducto(ProductoBancario pb){
+       Lista p = productos();
+       boolean borrado = false;
+       for( int i = 0 ; i < p.numero() && !borrado; i += 1 ){
+         ProductoBancario candidato = (ProductoBancario)p.objetoEn(i);
+         if( candidato().equals( pb ) ){
+           p.borra(i);
+           borrado = true;
+         }
+       }
+       return ret;
+    }
+
+    private int codigoLibrePara( String titular ){
+       Lista p = productosDeTitular(titular);
+       int maximo = 0;
+       for( int i = 0 ; i < p.numero(); i += 1 ){
+         ProductoBancario candidato = (ProductoBancario)p.objetoEn(i);
+         maximo = Math.max( maximo, candidato.codigo() );
+       }
+       return ret;
     }
 
     /**
@@ -37,6 +74,9 @@ public class TerminalBancario{
      * La cuenta comienza con saldo 0. El codigo debera calcularlo el propio metodo, para que no coincida con otro del titular
      */
     public Cuenta creaCuenta(String titular){
+       int codigo = codigoLibrePara(titular);
+       Cuenta ret = new Cuenta( titular, codigo );
+       return ret;
     }
 
 
@@ -45,6 +85,9 @@ public class TerminalBancario{
      * La tarjeta comienza con saldo 0. El codigo debera calcularlo el propio metodo, para que no coincida con otro del titular
      */
     public TarjetaDeCredito creaTarjeta(String titular){
+       int codigo = codigoLibrePara(titular);
+       TarjetaDeCredito ret = new TarjetaDeCredito( titular, codigo );
+       return ret;    
     }
 
     /**
@@ -52,5 +95,8 @@ public class TerminalBancario{
      * El deposito tiene el saldo indicado. El codigo debera calcularlo el propio metodo, para que no coincida con otro del titular
      */
     public Deposito creaDeposito(String titular, double saldo){
+       int codigo = codigoLibrePara(titular);
+       Deposito ret = new Deposito( titular, codigo, saldo );
+       return ret;
     }
 }
