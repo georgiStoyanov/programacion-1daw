6dc49c05a83235a8461d032020e7e92f310c06bf
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2015-01-26 13:15:29 +0100

    Sin caja

--- PR-Correcion-objetos-ii/src/main/java/banco/Menu.java
+++ PR-Correcion-objetos-ii/src/main/java/banco/Menu.java
@@ -8,35 +8,166 @@ public abstract class Menu extends Pantalla {
     private static int ALTO = 20;
     private static int ANCHO = 60;
 
-    abstract class Caja{
+    abstract class Caja {
         abstract char ESQUINA_ARRIBA_IZQUIERDA();
+
         abstract char ESQUINA_ARRIBA_DERECHA();
+
         abstract char ESQUINA_ABAJO_IZQUIERDA();
+
         abstract char ESQUINA_ABAJO_DERECHA();
+
         abstract char LADO_VERTICAL();
+
         abstract char LADO_HORIZONTAL();
-    }
-    
-    class CajaAscii extends Caja{
-        char ESQUINA_ARRIBA_IZQUIERDA(){return '/';};
-        char ESQUINA_ARRIBA_DERECHA(){return '\\';};
-        char ESQUINA_ABAJO_IZQUIERDA(){return'\\';};
-        char ESQUINA_ABAJO_DERECHA(){return'/';};
-        char LADO_VERTICAL(){return '|';};
-        char LADO_HORIZONTAL(){return '-';};
+
+        void lineaEnMarco(String s, PrintStream out) {
+            int anchoInterior = ANCHO - 5;
+            String linea = String.format("%c  %-" + (anchoInterior) + "s %c",
+                    caja.LADO_VERTICAL(),
+                    s.substring(0, Math.min(anchoInterior, s.length())),
+                    caja.LADO_VERTICAL());
+            out.println(linea);
+        }
+
+        void inicioMarco(String s, PrintStream out) {
+            int margen = (ANCHO - s.length()) / 2;
+            String linea = "" + caja.ESQUINA_ARRIBA_IZQUIERDA();
+            for (int i = 0; i < margen - 2; i++) {
+                linea += caja.LADO_HORIZONTAL();
+            }
+            linea += " " + s.toUpperCase() + " ";
+            for (int i = 0; linea.length() < ANCHO - 1; i++) {
+                linea += caja.LADO_HORIZONTAL();
+            }
+            linea += caja.ESQUINA_ARRIBA_DERECHA();
+            out.println(linea);
+        }
+
+        void finMarco(PrintStream out) {
+            out.print(caja.ESQUINA_ABAJO_IZQUIERDA());
+            for (int i = 0; i < ANCHO - 2; i++) {
+                out.print(caja.LADO_HORIZONTAL());
+            }
+            out.println(caja.ESQUINA_ABAJO_DERECHA());
+        }
+
+        void borrarPantalla(PrintStream out) {
+            for (int i = 0; i < ALTO * 2; i++) {
+                out.println();
+            }
+        }
     }
 
-    class CajaUnicode extends Caja{
-        char ESQUINA_ARRIBA_IZQUIERDA(){return '\u250c';};
-        char ESQUINA_ARRIBA_DERECHA(){return '\u2510';};
-        char ESQUINA_ABAJO_IZQUIERDA(){return'\u2514';};
-        char ESQUINA_ABAJO_DERECHA(){return'\u2518';};
-        char LADO_VERTICAL(){return '\u2502';};
-        char LADO_HORIZONTAL(){return '\u2500';};
+    class SinCaja extends Caja {
+
+        @Override
+        void lineaEnMarco(String s, PrintStream out) {
+            out.println(s);
+        }
+
+        @Override
+        void inicioMarco(String s, PrintStream out) {
+            out.println(s);
+        }
+
+        @Override
+        void finMarco(PrintStream out) {
+        }
+        
+        @Override
+        void borrarPantalla(PrintStream out) {
+            out.println();
+            out.println();
+            out.println();
+            out.println();
+        }
+
+        @Override
+        char ESQUINA_ARRIBA_IZQUIERDA() {
+            return 0;
+        }
+
+        @Override
+        char ESQUINA_ARRIBA_DERECHA() {
+            return 0;
+        }
+
+        @Override
+        char ESQUINA_ABAJO_IZQUIERDA() {
+            return 0;
+        }
+
+        @Override
+        char ESQUINA_ABAJO_DERECHA() {
+            return 0;
+        }
+
+        @Override
+        char LADO_VERTICAL() {
+            return 0;
+        }
+
+        @Override
+        char LADO_HORIZONTAL() {
+            return 0;
+        }
+
     }
-    
-    
-    private Caja caja = new CajaUnicode();
+
+    class CajaAscii extends Caja {
+        char ESQUINA_ARRIBA_IZQUIERDA() {
+            return '/';
+        };
+
+        char ESQUINA_ARRIBA_DERECHA() {
+            return '\\';
+        };
+
+        char ESQUINA_ABAJO_IZQUIERDA() {
+            return '\\';
+        };
+
+        char ESQUINA_ABAJO_DERECHA() {
+            return '/';
+        };
+
+        char LADO_VERTICAL() {
+            return '|';
+        };
+
+        char LADO_HORIZONTAL() {
+            return '-';
+        };
+    }
+
+    class CajaUnicode extends Caja {
+        char ESQUINA_ARRIBA_IZQUIERDA() {
+            return '\u250c';
+        };
+
+        char ESQUINA_ARRIBA_DERECHA() {
+            return '\u2510';
+        };
+
+        char ESQUINA_ABAJO_IZQUIERDA() {
+            return '\u2514';
+        };
+
+        char ESQUINA_ABAJO_DERECHA() {
+            return '\u2518';
+        };
+
+        char LADO_VERTICAL() {
+            return '\u2502';
+        };
+
+        char LADO_HORIZONTAL() {
+            return '\u2500';
+        };
+    }
+
+    private Caja caja = decideCaja();
 
     public static class Opcion {
         private Pantalla _accion;
@@ -73,6 +204,18 @@ public abstract class Menu extends Pantalla {
         return ret;
     }
 
+    private Caja decideCaja() {
+        if (false) {
+            return new SinCaja();
+        }
+        else {
+            if (System.lineSeparator().equals("\n")) {
+                return new CajaUnicode();
+            }
+            return new CajaAscii();
+        }
+    }
+
     private int toInt(String s) {
         try {
             return Integer.parseInt(s);
@@ -85,67 +228,30 @@ public abstract class Menu extends Pantalla {
     abstract public String[] cabecera();
 
     public void render(PrintStream out) {
-        borrarPantalla(out);
-        inicioMarco(nombre(), out);
+        caja.borrarPantalla(out);
+        caja.inicioMarco(nombre(), out);
 
-        lineaEnMarco(" ", out);
+        caja.lineaEnMarco(" ", out);
 
         String[] cs = cabecera();
         for (int i = 0; cs != null && i < cs.length; i++) {
-            lineaEnMarco(cs[i], out);
+            caja.lineaEnMarco(cs[i], out);
         }
 
-        lineaEnMarco(" ", out);
+        caja.lineaEnMarco(" ", out);
 
         Opcion[] ops = opciones();
         for (int i = 0; i < ops.length; i++) {
-            lineaEnMarco(String.format("%d .- %s", i, ops[i].nombre()), out);
+            caja.lineaEnMarco(String.format("%d .- %s", i, ops[i].nombre()), out);
         }
 
-        lineaEnMarco(" ", out);
+        caja.lineaEnMarco(" ", out);
 
-        finMarco(out);
+        caja.finMarco(out);
 
         out.println();
         out.println();
         out.println();
-    }
-
-    private void lineaEnMarco(String s, PrintStream out) {
-        int anchoInterior = ANCHO - 5;
-        String linea = String.format("%c  %-" + (anchoInterior) + "s %c",
-                caja.LADO_VERTICAL(),
-                s.substring(0, Math.min(anchoInterior, s.length())),
-                caja.LADO_VERTICAL());
-        out.println(linea);
-    }
-
-    private void inicioMarco(String s, PrintStream out) {
-        int margen = (ANCHO - s.length()) / 2;
-        String linea = "" + caja.ESQUINA_ARRIBA_IZQUIERDA();
-        for (int i = 0; i < margen - 2; i++) {
-            linea += caja.LADO_HORIZONTAL();
-        }
-        linea += " " + s.toUpperCase() + " ";
-        for (int i = 0; linea.length() < ANCHO - 1; i++) {
-            linea += caja.LADO_HORIZONTAL();
-        }
-        linea += caja.ESQUINA_ARRIBA_DERECHA();
-        out.println(linea);
-    }
-
-    private void finMarco(PrintStream out) {
-        out.print(caja.ESQUINA_ABAJO_IZQUIERDA());
-        for (int i = 0; i < ANCHO - 2; i++) {
-            out.print(caja.LADO_HORIZONTAL());
-        }
-        out.println(caja.ESQUINA_ABAJO_DERECHA());
-    }
-
-    private void borrarPantalla(PrintStream out) {
-        for (int i = 0; i < ALTO * 2; i++) {
-            out.println();
-        }
     }
 
     @Override
@@ -156,6 +262,5 @@ public abstract class Menu extends Pantalla {
     }
 
     abstract public Opcion[] opciones();
-    
 
 }
