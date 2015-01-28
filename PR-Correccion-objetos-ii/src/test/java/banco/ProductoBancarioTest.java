a403ff1da703be087c90ed8ebe1ec9970c99bcc0
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2015-01-28 13:45:35 +0100

    Un par de tests mas

--- PR-Correccion-objetos-ii/src/test/java/banco/ProductoBancarioTest.java
+++ PR-Correccion-objetos-ii/src/test/java/banco/ProductoBancarioTest.java
@@ -1,4 +1,5 @@
 package banco;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -10,122 +11,168 @@ import org.junit.Test;
 public class ProductoBancarioTest {
 
     @Test
-    public void productoBancarioDebeSerAbstract(){
-        boolean abstracta = Modifier.isAbstract( ProductoBancario.class.getModifiers() );
-        assertTrue( "La clase ProductoBancario debe ser abstracta", abstracta );
+    public void productoBancarioDebeSerAbstract() {
+        boolean abstracta = Modifier.isAbstract(ProductoBancario.class.getModifiers());
+        assertTrue("La clase ProductoBancario debe ser abstracta", abstracta);
     }
 
     @Test
-    public void equalsCuentaDepositoTrue(){
+    public void equalsCuentaDepositoTrue() {
         Cuenta c = new Cuenta("a", 0);
         Deposito d = new Deposito("a", 0, 1);
-        assertTrue( String.format("Deberian ser equals:%s y  %s", c, d ), c.equals(d) );
+        assertTrue(String.format("Deberian ser equals:%s y  %s", c, d), c.equals(d));
     }
 
     @Test
-    public void equalsCuentaDepositoFalseTitular(){
+    public void equalsCuentaDepositoFalseTitular() {
         Cuenta c = new Cuenta("a", 0);
         Deposito d = new Deposito("b", 0, 1);
-        assertTrue( String.format("No Deberian ser equals:%s y  %s", c, d ), !c.equals(d) );
+        assertTrue(String.format("No Deberian ser equals:%s y  %s", c, d), !c.equals(d));
     }
 
     @Test
-    public void equalsCuentaDepositoFalseCodigo(){
+    public void equalsCuentaDepositoFalseCodigo() {
         Cuenta c = new Cuenta("a", 0);
         Deposito d = new Deposito("a", 1, 1);
-        assertTrue( String.format("No Deberian ser equals:%s y  %s", c, d ), !c.equals(d) );
+        assertTrue(String.format("No Deberian ser equals:%s y  %s", c, d), !c.equals(d));
     }
 
     @Test
-    public void equalsCuentaTarjetaTrue(){
+    public void equalsCuentaTarjetaTrue() {
         Cuenta c = new Cuenta("a", 0);
         TarjetaDeCredito d = new TarjetaDeCredito("a", 0);
-        assertTrue( String.format("Deberian ser equals:%s y  %s", c, d ), c.equals(d) );
+        assertTrue(String.format("Deberian ser equals:%s y  %s", c, d), c.equals(d));
     }
 
     @Test
-    public void equalsCuentaTarjetaFalseTitular(){
+    public void equalsCuentaTarjetaFalseTitular() {
         Cuenta c = new Cuenta("a", 0);
         TarjetaDeCredito d = new TarjetaDeCredito("B", 0);
-        assertTrue( String.format("No Deberian ser equals:%s y  %s", c, d ), !c.equals(d) );
+        assertTrue(String.format("No Deberian ser equals:%s y  %s", c, d), !c.equals(d));
     }
 
     @Test
-    public void equalsCuentaTarjetaFalseCodigo(){
+    public void equalsCuentaTarjetaFalseCodigo() {
         Cuenta c = new Cuenta("a", 0);
         TarjetaDeCredito d = new TarjetaDeCredito("a", 1);
-        assertTrue( String.format("No Deberian ser equals:%s y  %s", c, d ), !c.equals(d) );
+        assertTrue(String.format("No Deberian ser equals:%s y  %s", c, d), !c.equals(d));
     }
 
     @Test
-    public void laCuentaSeCreaACero(){
-        assertTrue( "El saldo inicial de una cuenta es cero", new Cuenta("a",0).getSaldo() == 0 );
+    public void equalsDepositoTarjetaTrue() {
+        Deposito c = new Deposito("a", 0, 10);
+        TarjetaDeCredito d = new TarjetaDeCredito("a", 0);
+        assertTrue(String.format("Deberian ser equals:%s y  %s", c, d), c.equals(d));
     }
 
     @Test
-    public void laTarjetaSeCreaACero(){
-        assertTrue( "El saldo inicial de una tarjeta es cero", new TarjetaDeCredito("a",0).getSaldo() == 0 );
+    public void equalsDepositoTarjetaFalseTitular() {
+        Deposito c = new Deposito("a", 0, 10);
+        TarjetaDeCredito d = new TarjetaDeCredito("B", 0);
+        assertTrue(String.format("No Deberian ser equals:%s y  %s", c, d), !c.equals(d));
     }
+
+    @Test
+    public void equalsDepositoTarjetaFalseCodigo() {
+        Deposito c = new Deposito("a", 0, 10);
+        TarjetaDeCredito d = new TarjetaDeCredito("a", 1);
+        assertTrue(String.format("No Deberian ser equals:%s y  %s", c, d), !c.equals(d));
+    }
+
+    @Test
+    public void laCuentaSeCreaACero() {
+        assertTrue("El saldo inicial de una cuenta es cero", new Cuenta("a", 0).getSaldo() == 0);
+    }
+
+    @Test
+    public void laTarjetaSeCreaACero() {
+        assertTrue("El saldo inicial de una tarjeta es cero", new TarjetaDeCredito("a", 0).getSaldo() == 0);
+    }
+
     
-    @Test( expected = Exception.class)
-    public void elDepositoNoSeCreaConSaldoNegativo(){
+    @Test
+    public void elDepositoSeCreaConSaldoIndicado() {
+        Deposito d = new Deposito( "pepe", 100, 100 );
+        assertTrue("El saldo inicial se puso a 100:" + d , d.getSaldo() == 100);
+    }
+
+    @Test(expected = Exception.class)
+    public void elDepositoNoSeCreaConSaldoNegativo() {
         new Deposito("a", 0, -1);
     }
 
-    @Test( expected = Exception.class)
-    public void elDepositoNoSePuedeModificarPositivo(){
+    @Test(expected = Exception.class)
+    public void elDepositoNoSePuedeModificarPositivo1() {
         new Deposito("a", 0, 0).movimientoSaldo(1);
     }
 
-    @Test( expected = Exception.class)
-    public void elDepositoNoSePuedeModificarNegativo(){
+    @Test(expected = Exception.class)
+    public void elDepositoNoSePuedeModificarPositivo2() {
+        new Deposito("a", 0, 10).movimientoSaldo(1);
+    }
+
+    
+    @Test(expected = Exception.class)
+    public void elDepositoNoSePuedeModificarNegativo1() {
         new Deposito("a", 0, 0).movimientoSaldo(-1);
     }
 
-    @Test( expected = Exception.class)
-    public void laCuentaNoPuedeTenerDescubierto(){
+    @Test(expected = Exception.class)
+    public void elDepositoNoSePuedeModificarNegativo2() {
+        new Deposito("a", 0, 10).movimientoSaldo(-1);
+    }
+
+    @Test(expected = Exception.class)
+    public void laCuentaNoPuedeTenerDescubierto() {
         new Cuenta("a", 0).movimientoSaldo(-1);
     }
 
-    @Test( expected = Exception.class)
-    public void laTarjetaNoPuedeModificarPositivo(){
+    @Test(expected = Exception.class)
+    public void laTarjetaNoPuedeModificarPositivo() {
         new TarjetaDeCredito("a", 0).movimientoSaldo(1);
     }
 
     @Test
-    public void laTarjetaPuedeModificarNegativo(){
+    public void laTarjetaPuedeModificarNegativo() {
         ProductoBancario b = new TarjetaDeCredito("a", 0);
         b.movimientoSaldo(-1);
         b.movimientoSaldo(-1);
-        assertTrue( "El saldo tras -1 y -1 deberia ser -2: " + b , b.getSaldo() == -2 );
+        assertTrue("El saldo tras -1 y -1 deberia ser -2: " + b, b.getSaldo() == -2);
+    }
+
+    @Test(expected = Exception.class)
+    public void laTarjetaSoloPuedeModificarNegativo() {
+        ProductoBancario b = new TarjetaDeCredito("a", 0);
+        b.movimientoSaldo(-100);
+        b.movimientoSaldo(1);
     }
 
     @Test
-    public void laCuentaPuedeModificarPositivoYNegativo(){
+    public void laCuentaPuedeModificarPositivoYNegativo() {
         ProductoBancario b = new Cuenta("a", 0);
         b.movimientoSaldo(100);
         b.movimientoSaldo(-1);
-        assertTrue( "El saldo tras 100 y -1 deberia ser 99: " + b , b.getSaldo() == 99 );
+        assertTrue("El saldo tras 100 y -1 deberia ser 99: " + b, b.getSaldo() == 99);
     }
-    
-    @Test( expected = Exception.class)
-    public void laCuentaPuedeModificarPositivoYNegativoPeroNoDescubierto(){
+
+    @Test(expected = Exception.class)
+    public void laCuentaPuedeModificarPositivoYNegativoPeroNoDescubierto() {
         ProductoBancario b = new Cuenta("a", 0);
         b.movimientoSaldo(100);
         b.movimientoSaldo(100);
         b.movimientoSaldo(-1);
-        assertTrue( "El saldo tras 100, 100 y -1 deberia ser 199: " + b , b.getSaldo() == 199 );
+        assertTrue("El saldo tras 100, 100 y -1 deberia ser 199: " + b, b.getSaldo() == 199);
         b.movimientoSaldo(-200);
     }
 
     @Test
-    public void laCuentaPuedeQuedarACero(){
+    public void laCuentaPuedeQuedarACero() {
         ProductoBancario b = new Cuenta("a", 0);
         b.movimientoSaldo(100);
         b.movimientoSaldo(100);
         b.movimientoSaldo(-1);
         b.movimientoSaldo(-199);
-        assertTrue( "El saldo tras 100, 100 ,-1 m -199 deberia ser 0: " + b , b.getSaldo() == 0 );
-    }
+        assertTrue("El saldo tras 100, 100 ,-1 m -199 deberia ser 0: " + b, b.getSaldo() == 0);
+     }
 
 }
