a3dbc334b6adfca0b57265e56009e6dc84fec694
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2015-04-27 13:04:42 +0200

    Las direcciones casi están

--- ejemplos-clase-evaluacion-3/Hibernate/src/app/GUI.java
+++ ejemplos-clase-evaluacion-3/Hibernate/src/app/GUI.java
@@ -36,6 +36,8 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.TableModel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class GUI extends JFrame {
 
@@ -44,6 +46,106 @@ public class GUI extends JFrame {
     private JTable clientesTable;
     private ClientesTableModel _clientesTableModel;
     private EntityManager _entityManager;
+    private JTextField nombreText;
+    private JTextField apellidosText;
+    private JTable direccionesTable;
+
+    private class DireccionesTableModel implements TableModel {
+
+        private Cliente _cliente;
+        private List<Direccion> _direcciones;
+        private String[] COLUMNAS = { "Dirección", "C. Postal", "Municipio", "Provincia" };
+
+        public DireccionesTableModel(Cliente c) {
+            _cliente = c;
+            _direcciones = _cliente.getDirecciones();
+        }
+
+        @Override
+        public int getRowCount() {
+            return _direcciones.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return COLUMNAS.length;
+        }
+
+        @Override
+        public String getColumnName(int columnIndex) {
+            return COLUMNAS[columnIndex];
+        }
+
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            return String.class;
+        }
+
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return true;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            Direccion d = _direcciones.get(rowIndex);
+            switch (columnIndex) {
+            case 0:
+                return d.getDireccion();
+            case 1:
+                return d.getCodigoPostal();
+            case 2:
+                return d.getMunicipio();
+            case 3:
+                return d.getProvincia();
+            default:
+                return "???";
+            }
+        }
+
+        @Override
+        public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+            String v = aValue.toString();
+            Direccion d = _direcciones.get(rowIndex);
+            switch (columnIndex) {
+            case 0:
+                d.setDireccion(v);
+                break;
+            case 1:
+                d.setCodigoPostal(v);
+                break;
+            case 2:
+                d.setMunicipio(v);
+                break;
+            case 3:
+                d.setProvincia(v);
+                break;
+            default:
+                throw new IllegalArgumentException();
+            }
+            salvarDireccion(d);
+            fireTableChanged( new TableModelEvent(this) );
+        }
+
+        private List<TableModelListener> _listeners = new ArrayList<TableModelListener>();
+
+        private void fireTableChanged(TableModelEvent e) {
+            for (TableModelListener l : _listeners) {
+                l.tableChanged(e);
+            }
+        }
+
+        @Override
+        public void addTableModelListener(TableModelListener l) {
+            _listeners.add(l);
+        }
+
+        @Override
+        public void removeTableModelListener(TableModelListener l) {
+            _listeners.remove(l);
+        }
+
+    }
 
     private class ClientesTableModel implements TableModel {
 
@@ -74,7 +176,6 @@ public class GUI extends JFrame {
             }
 
         }
-
 
         @Override
         public int getRowCount() {
@@ -180,10 +281,10 @@ public class GUI extends JFrame {
         initComponents();
         initListeners();
     }
-    
-    private void initComponents(){
+
+    private void initComponents() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
+        setBounds(100, 100, 781, 435);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(new BorderLayout(0, 0));
@@ -199,60 +300,141 @@ public class GUI extends JFrame {
         clientesTable = new JTable();
         scrollPane.setViewportView(clientesTable);
 
-        JPanel panel = new JPanel();
+        JPanel datosCliente = new JPanel();
 
-        clientesPanel.add(panel, BorderLayout.SOUTH);
+        clientesPanel.add(datosCliente, BorderLayout.SOUTH);
 
-        GridBagLayout gbl_panel = new GridBagLayout();
-        gbl_panel.columnWidths = new int[] { 64, 0 };
-        gbl_panel.rowHeights = new int[] { 64, 0, 0 };
-        gbl_panel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
-        gbl_panel.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
-        panel.setLayout(gbl_panel);
+        GridBagLayout gbl_datosCliente = new GridBagLayout();
+        gbl_datosCliente.columnWeights = new double[] { 1.0, 0.0 };
+
+        datosCliente.setLayout(gbl_datosCliente);
+
+        JPanel datosTextoCliente = new JPanel();
+        GridBagConstraints gbc_datosTextoCliente = new GridBagConstraints();
+        gbc_datosTextoCliente.gridheight = 2;
+        gbc_datosTextoCliente.weighty = 1.0;
+        gbc_datosTextoCliente.weightx = 1.0;
+        gbc_datosTextoCliente.insets = new Insets(0, 0, 5, 5);
+        gbc_datosTextoCliente.fill = GridBagConstraints.BOTH;
+        gbc_datosTextoCliente.gridx = 0;
+        gbc_datosTextoCliente.gridy = 0;
+        datosCliente.add(datosTextoCliente, gbc_datosTextoCliente);
+        GridBagLayout gbl_datosTextoCliente = new GridBagLayout();
+        gbl_datosTextoCliente.columnWidths = new int[] { 0, 0, 0 };
+        gbl_datosTextoCliente.rowHeights = new int[] { 0, 0, 0, 0 };
+        gbl_datosTextoCliente.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+        gbl_datosTextoCliente.rowWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
+        datosTextoCliente.setLayout(gbl_datosTextoCliente);
+
+        JLabel lblNewLabel = new JLabel("Nombre");
+        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+        gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel.gridx = 0;
+        gbc_lblNewLabel.gridy = 0;
+        datosTextoCliente.add(lblNewLabel, gbc_lblNewLabel);
+
+        nombreText = new JTextField();
+        GridBagConstraints gbc_nombreText = new GridBagConstraints();
+        gbc_nombreText.weightx = 1.0;
+        gbc_nombreText.insets = new Insets(0, 0, 5, 0);
+        gbc_nombreText.fill = GridBagConstraints.HORIZONTAL;
+        gbc_nombreText.gridx = 1;
+        gbc_nombreText.gridy = 0;
+        datosTextoCliente.add(nombreText, gbc_nombreText);
+        nombreText.setColumns(10);
+
+        JLabel lblApellidos = new JLabel("Apellidos");
+        GridBagConstraints gbc_lblApellidos = new GridBagConstraints();
+        gbc_lblApellidos.anchor = GridBagConstraints.EAST;
+        gbc_lblApellidos.insets = new Insets(0, 0, 5, 5);
+        gbc_lblApellidos.gridx = 0;
+        gbc_lblApellidos.gridy = 1;
+        datosTextoCliente.add(lblApellidos, gbc_lblApellidos);
+
+        apellidosText = new JTextField();
+        GridBagConstraints gbc_apellidosText = new GridBagConstraints();
+        gbc_apellidosText.insets = new Insets(0, 0, 5, 0);
+        gbc_apellidosText.fill = GridBagConstraints.HORIZONTAL;
+        gbc_apellidosText.gridx = 1;
+        gbc_apellidosText.gridy = 1;
+        datosTextoCliente.add(apellidosText, gbc_apellidosText);
+        apellidosText.setColumns(10);
+
+        JLabel lblDirecciones = new JLabel("Direcciones");
+        GridBagConstraints gbc_lblDirecciones = new GridBagConstraints();
+        gbc_lblDirecciones.insets = new Insets(0, 0, 0, 5);
+        gbc_lblDirecciones.gridx = 0;
+        gbc_lblDirecciones.gridy = 2;
+        datosTextoCliente.add(lblDirecciones, gbc_lblDirecciones);
+
+        JScrollPane scrollPane_1 = new JScrollPane();
+        GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+        gbc_scrollPane_1.weighty = 1.0;
+        gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+        gbc_scrollPane_1.gridx = 1;
+        gbc_scrollPane_1.gridy = 2;
+        datosTextoCliente.add(scrollPane_1, gbc_scrollPane_1);
+
+        direccionesTable = new JTable();
+        direccionesTable.setPreferredScrollableViewportSize(new Dimension(100, 100));
+        scrollPane_1.setViewportView(direccionesTable);
+
+        JPanel panelConImagen = new JPanel();
+        GridBagConstraints gbc_panelConImagen = new GridBagConstraints();
+        gbc_panelConImagen.anchor = GridBagConstraints.NORTH;
+        gbc_panelConImagen.insets = new Insets(0, 0, 5, 5);
+        gbc_panelConImagen.gridx = 1;
+        gbc_panelConImagen.gridy = 0;
+        datosCliente.add(panelConImagen, gbc_panelConImagen);
+        GridBagLayout gbl_panelConImagen = new GridBagLayout();
+        gbl_panelConImagen.columnWeights = new double[] { 0.0 };
+        gbl_panelConImagen.rowWeights = new double[] { 0.0, 0.0 };
+        panelConImagen.setLayout(gbl_panelConImagen);
 
         imagenIcon = new JButton("New button");
+        GridBagConstraints gbc_imagenIcon = new GridBagConstraints();
+        gbc_imagenIcon.anchor = GridBagConstraints.NORTH;
+        gbc_imagenIcon.insets = new Insets(0, 0, 5, 5);
+        gbc_imagenIcon.gridx = 0;
+        gbc_imagenIcon.gridy = 0;
+        panelConImagen.add(imagenIcon, gbc_imagenIcon);
         imagenIcon.setPreferredSize(new Dimension(64, 64));
         imagenIcon.setMinimumSize(new Dimension(64, 64));
         imagenIcon.setMaximumSize(new Dimension(64, 64));
-        GridBagConstraints gbc_imagenIcon = new GridBagConstraints();
-        gbc_imagenIcon.insets = new Insets(0, 0, 5, 0);
-        gbc_imagenIcon.gridx = 0;
-        gbc_imagenIcon.gridy = 0;
-        panel.add(imagenIcon, gbc_imagenIcon);
 
         JButton cambiarImagenButton = new JButton("Cambiar...");
+        GridBagConstraints gbc_cambiarImagenButton = new GridBagConstraints();
+        gbc_cambiarImagenButton.insets = new Insets(0, 0, 0, 5);
+        gbc_cambiarImagenButton.gridx = 0;
+        gbc_cambiarImagenButton.gridy = 1;
+        panelConImagen.add(cambiarImagenButton, gbc_cambiarImagenButton);
         cambiarImagenButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 cambiarImagen();
             }
         });
-        GridBagConstraints gbc_cambiarImagenButton = new GridBagConstraints();
-        gbc_cambiarImagenButton.weightx = 1.0;
-        gbc_cambiarImagenButton.fill = GridBagConstraints.HORIZONTAL;
-        gbc_cambiarImagenButton.anchor = GridBagConstraints.NORTH;
-        gbc_cambiarImagenButton.gridx = 0;
-        gbc_cambiarImagenButton.gridy = 1;
-        panel.add(cambiarImagenButton, gbc_cambiarImagenButton);
-        
+
     }
 
-    
-    private void initListeners(){
-        clientesTable.getSelectionModel().addListSelectionListener( new ListSelectionListener() {
-            
+    private void initListeners() {
+        clientesTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 Cliente c = getClienteSeleccionado();
-                if( c == null || c.getImagen() == null ){
+                if (c == null || c.getImagen() == null) {
                     imagenIcon.setIcon(null);
                     return;
                 }
                 Icon icon = new ImageIcon(c.getImagen());
                 imagenIcon.setIcon(icon);
+                
+                direccionesTable.setModel( new DireccionesTableModel(c) );
             }
         });
     }
-    
+
     protected void cambiarImagen() {
         Cliente c = getClienteSeleccionado();
         if (c == null) {
@@ -312,16 +494,24 @@ public class GUI extends JFrame {
         }
         return _entityManager;
     }
-    
+
     private void salvarCliente(Cliente c) {
+        salvarObjeto(c);
+    }
+    
+    private void salvarObjeto(Object o){
         EntityTransaction tx = getEntityManager().getTransaction();
         try {
             tx.begin();
-            getEntityManager().persist(c);
+            getEntityManager().persist(o);
         }
         finally {
             tx.commit();
         }
+    }
+    
+    private void salvarDireccion(Direccion d) {
+        salvarObjeto(d);
     }
 
 }
